3e85622e419f1a4c09ee2acc237c63e1f15c7492
Du Vu <hoaivubk@gmail.com>
2016-03-10 13:58:24 +0700

    Refactor function Sorting for performance

--- app/manage-tracker/manage-tracker.js
+++ app/manage-tracker/manage-tracker.js
@@ -1,45 +1,53 @@
-﻿appCtrls.controller('ListTrackerCtrl', function ($scope, rootSvc, localDbSvc, webSvc) {
-    rootSvc.SetPageTitle('List Trackers');
-    rootSvc.SetActiveMenu('Tracker');
-    rootSvc.SetPageHeader("Trackers");
+﻿appCtrls.controller('ListTrackerCtrl',
+    function ($scope, $filter, rootSvc, localDbSvc, webSvc) {
+        rootSvc.SetPageTitle('List Trackers');
+        rootSvc.SetActiveMenu('Tracker');
+        rootSvc.SetPageHeader("Trackers");
 
-    $scope.AuthToken = localDbSvc.getToken();
+        $scope.AuthToken = localDbSvc.getToken();
 
-    var BindTrackerList = function () {
-        webSvc.getDevices($scope.PageSize, $scope.PageIndex, $scope.Sc, $scope.So).success(function(data){
-            console.log("TRACKER", data);
-            if (data.status.code == 0) {
-                $scope.TrackerList = data.response;
-                $scope.TrackerList.totalCount = data.totalCount;
-                console.log(data);
-            }
-        });
-    }
+        var BindTrackerList = function () {
+            webSvc.getDevices($scope.PageSize, $scope.PageIndex, $scope.Sc, $scope.So).success(function(data){
+                console.log("TRACKER", data);
+                if (data.status.code == 0) {
+                    $scope.TrackerList = data.response;
+                    $scope.TrackerList.totalCount = data.totalCount;
+                    console.log(data);
+                }
+            });
+        }
 
-    $scope.Init = function () {
-        $scope.PageSize = '20';
-        $scope.PageIndex = 1;
-        $scope.So = "asc";
-        $scope.Sc = "name";
-        BindTrackerList();
-    }
+        $scope.Init = function () {
+            $scope.PageSize = '20';
+            $scope.PageIndex = 1;
+            $scope.So = "asc";
+            $scope.Sc = "name";
+            BindTrackerList();
+        }
 
-    $scope.PageSizeChanged = function () {
-        BindTrackerList();
-    }
+        $scope.PageSizeChanged = function () {
+            BindTrackerList();
+        }
 
-    $scope.PageChanged = function (page) {
-        $scope.PageIndex = page;
-        BindTrackerList();
-    }
+        $scope.PageChanged = function (page) {
+            $scope.PageIndex = page;
+            BindTrackerList();
+        }
 
-    $scope.Sorting = function (expression) {
-        $scope.So = $scope.So == "asc" ? "desc" : "asc";
-        $scope.Sc = expression;
-        console.log($scope.So);
-        console.log($scope.Sc);
-        BindTrackerList();
-    }
+        /*$scope.Sorting = function (expression) {
+            $scope.So = $scope.So == "asc" ? "desc" : "asc";
+            $scope.Sc = expression;
+            console.log($scope.So);
+            console.log($scope.Sc);
+            BindTrackerList();
+        }*/
+
+        var orderBy = $filter('orderBy');
+        $scope.Sorting = function(predicate) {
+            $scope.predicate = predicate;
+            $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+            $scope.TrackerList = orderBy($scope.TrackerList, predicate, $scope.reverse);
+        };
 });
 /**
  * Created by beou on 09/03/2016.
