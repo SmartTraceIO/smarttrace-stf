81e8f37f475794bbcecc81778305952b05d40748
Du Vu <hoaivubk@gmail.com>
2016-06-16 00:25:42 +0700

    #346 fixed bug not reset trackerList

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,126 +16,6 @@ module.exports = function(grunt) {
                         cwd: 'app',
                         src: '**/*.js',
                         dest: '<%= distdir %>/app'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/autostart-template',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/autostart-template'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/change-password',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/change-password'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/config',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/config'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/forget-password',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/forget-password'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/global',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/global'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/login',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/login'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-alert',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-alert'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-group',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-group'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-location',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-location'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-notification',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-notification'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-shipment-template',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-shipment-template'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-tracker',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-tracker'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/manage-user',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/manage-user'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/new-shipment',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/new-shipment'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/preference',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/preference'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/simulator',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/simulator'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/user-update',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/user-update'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/view-shipment',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/view-shipment'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/view-shipment-detail-share',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/view-shipment-detail-share'
-                    },
-                    {
-                        expand:true,
-                        cwd: 'app/view-shipment-detail-table',
-                        src: '**/*.js',
-                        dest: '<%= distdir %>/app/view-shipment-detail-table'
                     }
                 ],
             }
@@ -154,9 +34,33 @@ module.exports = function(grunt) {
             },
             html: {
                 files: [
-                    {expand: true, cwd: 'app/', src: ['**/*.html'], dest: '<%= distdir %>/app'},
                     {expand: true, cwd: 'app/', src: ['**/*.json'], dest: '<%= distdir %>/app'},
                 ]
+            }
+        },
+    //    htmlmin
+        htmlmin : {
+            dist: {
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/',
+                    src: ['**/*.html'],
+                    dest: '<%= distdir %>/app'
+                }]
+
+            },
+            my_index: {
+                options: {                                 // Target options
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    '<%= distdir %>/index.html': 'index.html'
+                }
             }
         }
     });
@@ -164,8 +68,9 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     // Default task(s).
-    grunt.registerTask('build', ['copy', 'uglify']);
+    grunt.registerTask('build', ['copy', 'uglify', 'htmlmin']);
     grunt.registerTask('default', ['build']);
 
 };
