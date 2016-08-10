0fe3a4626857dd4a176eea9151f18b27a3bcd5d5
Du Vu <hoaivubk@gmail.com>
2016-08-10 16:46:54 +0700

    #400 Fixed allow shutdown/deactive "Default"

--- app/config/appConfig.js
+++ app/config/appConfig.js
@@ -126,8 +126,8 @@ app.config(['$locationProvider', '$stateProvider', '$controllerProvider', '$prov
     $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
 
     $httpProvider.defaults.useXDomain = true;
-    //delete $httpProvider.defaults.headers.common['Access-Control-Allow-Methods'];
-    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    delete $httpProvider.defaults.headers.common['Access-Control-Allow-Methods'];
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
     
     $httpProvider.interceptors.push([
         '$injector',
