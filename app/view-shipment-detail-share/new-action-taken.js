862709254ada4ed0b047bc37c34e8d9e23d0f4d2
Vyacheslav Soldatov <vyacheslav.soldatov@inbox.ru>
2017-07-18 20:37:01 +0300

    Commited and merged new action talen list

--- app/view-shipment-detail-share/new-action-taken.js
+++ app/view-shipment-detail-share/new-action-taken.js
@@ -2,23 +2,14 @@
  * Created by beou on 31/05/2016.
  */
 appCtrls.controller('NewActionTakenController', NewActionTakenController);
-function NewActionTakenController($uibModalInstance, $scope, webSvc, currentAlert, actionList) {
-    $scope.alert = {
-        type: 'Hot',
-        description: '> 5C for more than 10mins',
-        timeStr: '2012/07/12'
-    };
-var a1={action: 'Action 1',requestVerification: true};
-
-    $scope.actions = [
-        a1,
-        {action: 'Action 2',requestVerification: false}
-    ];
+function NewActionTakenController($uibModalInstance, $scope, rootScope, currentAlert, actionList) {
+    $scope.alert = currentAlert;
+    $scope.actions = actionList;
 
     $scope.actionTaken = {
-        action: a1,
-        comments: 'any comment',
-        date: new Date()
+        action: null,
+        comments: null,
+        date: null
     };
 
     //-- cancel
@@ -27,6 +18,6 @@ var a1={action: 'Action 1',requestVerification: true};
     };
     
     $scope.saveActionTaken = function() {
-    	$scope.cancel();
+        rootScope.saveActionTaken($scope.actionTaken);
     };
 }
